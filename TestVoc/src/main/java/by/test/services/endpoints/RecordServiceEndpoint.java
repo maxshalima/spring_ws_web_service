62fbc095a5bd89a4e9bff1ddbab02d79f104c497
maxshalima <maxshalima@gmail.com>
2014-06-02 16:29:20 +0300

    add other endpoints

--- TestVoc/src/main/java/by/test/services/endpoints/RecordServiceEndpoint.java
+++ TestVoc/src/main/java/by/test/services/endpoints/RecordServiceEndpoint.java
@@ -10,6 +10,12 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import by.test.services.RecordService;
 import by.test.webservices.VocabloryRecord;
+import by.test.webservices.recordservice.AddRecordToVocabloryRequest;
+import by.test.webservices.recordservice.AddRecordToVocabloryResponse;
+import by.test.webservices.recordservice.GetAllVocabloriesRequest;
+import by.test.webservices.recordservice.GetAllVocabloriesResponse;
+import by.test.webservices.recordservice.GetNumberOfRecordsRequest;
+import by.test.webservices.recordservice.GetNumberOfRecordsResponse;
 import by.test.webservices.recordservice.SearchRecordsRequest;
 import by.test.webservices.recordservice.SearchRecordsResponse;
 
@@ -27,6 +33,38 @@ public class RecordServiceEndpoint {
 		List<VocabloryRecord> vocabloryRecords = recordService_i
 				.searchRecordsLessThan(request.getContent());
 		response.getRecordList().addAll(vocabloryRecords);
+		return response;
+	}
+
+	@PayloadRoot(localPart = "GetAllVocabloriesRequest", namespace = TARGET_NAMESPACE)
+	public @ResponsePayload
+	GetAllVocabloriesResponse getAllVocablories(
+			@RequestPayload GetAllVocabloriesRequest request) throws Exception {
+		GetAllVocabloriesResponse response = new GetAllVocabloriesResponse();
+		List<String> vocabloryList = recordService_i.getAllVocablories();
+		response.getVocabloriesList().addAll(vocabloryList);
+		return response;
+	}
+
+	@PayloadRoot(localPart = "GetNumberOfRecordsRequest", namespace = TARGET_NAMESPACE)
+	public @ResponsePayload
+	GetNumberOfRecordsResponse getNumberOfRecord(
+			@RequestPayload GetNumberOfRecordsRequest request) throws Exception {
+		GetNumberOfRecordsResponse response = new GetNumberOfRecordsResponse();
+		Long numberOfRecords = recordService_i.getNumberOfRecords();
+		response.setRecordNumber(numberOfRecords);
+		return response;
+	}
+
+	@PayloadRoot(localPart = "AddRecordToVocabloryRequest", namespace = TARGET_NAMESPACE)
+	public @ResponsePayload
+	AddRecordToVocabloryResponse addRecordToVoc(
+			@RequestPayload AddRecordToVocabloryRequest request)
+			throws Exception {
+		AddRecordToVocabloryResponse response = new AddRecordToVocabloryResponse();
+		boolean isSuccessed = recordService_i.addRecordToVoc(
+				request.getVocabloryName(), request.getContent());
+		response.setIsSuccessed(isSuccessed);
 		return response;
 	}
 
