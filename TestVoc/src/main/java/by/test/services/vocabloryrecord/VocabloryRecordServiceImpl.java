e5f1612b0016ea1217af5840eaf5c4a1c7fe416f
maxshalima <maxshalima@gmail.com>
2014-06-04 20:48:49 +0300

    some small code clean

--- TestVoc/src/main/java/by/test/services/vocabloryrecord/VocabloryRecordServiceImpl.java
+++ TestVoc/src/main/java/by/test/services/vocabloryrecord/VocabloryRecordServiceImpl.java
@@ -11,43 +11,41 @@ import org.springframework.stereotype.Service;
 
 import ru.blogic.test.storage.Storage.Dictionary.Record;
 import ru.blogic.test.storage.Storage.Dictionary.RecordAttr;
-import by.test.services.dictionary.DictionaryService;
 import by.test.services.record.RecordAccessService;
 import by.test.webservices.VocabloryRecord;
 
 @Service
 public class VocabloryRecordServiceImpl implements VocabloryRecordService {
 
-    @Autowired
-    private RecordAccessService recordAccessService_i;
+	@Autowired
+	private RecordAccessService recordAccessService_i;
 
-    @Autowired
-    private DictionaryService dictionaryService_i;
+	@Override
+	@Cacheable(value = "allSortedVocabloryRecords", key = "#aVocName")
+	public List<VocabloryRecord> getGetAllSortedVocabloryRecords(String aVocName)
+			throws Exception {
+		List<String> allRecordsIds = recordAccessService_i
+				.getAllRecordsIds(aVocName);
+		List<VocabloryRecord> allRecords = new ArrayList<VocabloryRecord>();
 
-    @Override
-    @Cacheable(value = "allSortedVocabloryRecords", key = "#aVocName")
-    public List<VocabloryRecord> getGetAllSortedVocabloryRecords(String aVocName) throws Exception {
-        List<String> allRecordsIds = recordAccessService_i.getAllRecordsIds(aVocName);
-        List<VocabloryRecord> allRecords = new ArrayList<VocabloryRecord>();
+		for (String recId : allRecordsIds) {
+			Record record = recordAccessService_i.getRecord(aVocName, recId);
+			VocabloryRecord vocabloryRecord = new VocabloryRecord();
+			vocabloryRecord.setID(record.getId());
+			vocabloryRecord.setContent(Long.parseLong((String) record
+					.getAttributes().get(RecordAttr.CONTENT)));
+			allRecords.add(vocabloryRecord);
+		}
 
-        for (String recId : allRecordsIds) {
-            Record record = recordAccessService_i.getRecord(aVocName, recId);
-            VocabloryRecord vocabloryRecord = new VocabloryRecord();
-            vocabloryRecord.setID(record.getId());
-            vocabloryRecord.setContent(Long.parseLong((String) record.getAttributes().get(RecordAttr.CONTENT)));
-            allRecords.add(vocabloryRecord);
-        }
+		Collections.sort(allRecords, new Comparator<VocabloryRecord>() {
+			@Override
+			public int compare(VocabloryRecord record1, VocabloryRecord record2) {
+				return ((Long) record1.getContent()).compareTo((Long) record2
+						.getContent());
+			}
+		});
 
-        Collections.sort(allRecords, new Comparator<VocabloryRecord>() {
-            @Override
-            public int compare(VocabloryRecord record1, VocabloryRecord record2) {
-                Long conten1 = record1.getContent();
-                Long conten2 = record2.getContent();
-                return conten1.compareTo(conten2);
-            }
-        });
-
-        return allRecords;
-    }
+		return allRecords;
+	}
 
 }
