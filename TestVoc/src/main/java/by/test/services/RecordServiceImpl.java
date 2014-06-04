fcbc1f7f50ad173e358b9bb2c3c9121e29dba37f
maxshalima <maxshalima@gmail.com>
2014-06-04 21:51:52 +0300

    some really very small code optimization

--- TestVoc/src/main/java/by/test/services/RecordServiceImpl.java
+++ TestVoc/src/main/java/by/test/services/RecordServiceImpl.java
@@ -20,72 +20,76 @@ import by.test.webservices.VocabloryRecord;
 @Service
 public class RecordServiceImpl implements RecordService {
 
-    @Autowired
-    private RecordAccessService recordAccessService_i;
+	@Autowired
+	private RecordAccessService recordAccessService_i;
 
-    @Autowired
-    private DictionaryService dictionaryService_i;
+	@Autowired
+	private DictionaryService dictionaryService_i;
 
-    @Autowired
-    private VocabloryRecordService vocabloryRecordService_i;
+	@Autowired
+	private VocabloryRecordService vocabloryRecordService_i;
 
-    @Override
-    public List<String> getAllVocabloryNames() throws Exception {
-        return dictionaryService_i.getDictionariesNames();
-    }
+	@Override
+	public List<String> getAllVocabloryNames() throws Exception {
+		return dictionaryService_i.getDictionariesNames();
+	}
 
-    @Override
-    @Cacheable("numberOfAllRecords")
-    public String getNumberOfRecords() throws Exception {
-        BigInteger count = BigInteger.valueOf(0);
-        List<String> allVocNameList = dictionaryService_i.getDictionariesNames();
+	@Override
+	@Cacheable("numberOfAllRecords")
+	public String getNumberOfRecords() throws Exception {
+		BigInteger count = BigInteger.valueOf(0);
+		List<String> allVocNameList = dictionaryService_i
+				.getDictionariesNames();
 
-        for (String vocName : allVocNameList) {
-            int dictCount = recordAccessService_i.getRecordCount(vocName);
-            count = count.add(BigInteger.valueOf(dictCount));
-        }
-        return count.toString();
-    }
+		for (String vocName : allVocNameList) {
+			int dictCount = recordAccessService_i.getRecordCount(vocName);
+			count = count.add(BigInteger.valueOf(dictCount));
+		}
+		return count.toString();
+	}
 
-    //    @Caching(evict = { @CacheEvict(value = "numberOfAllRecords", allEntries = true),
-    //            @CacheEvict(value = "dictionary", key = "#aVocName"),
-    //            @CacheEvict(value = "allRecordIdsFromVoc", key = "#aVocName"),
-    //            @CacheEvict(value = "recordCount", key = "#aVocName"),
-    //            @CacheEvict(value = "allSortedVocabloryRecords", key = "#aVocName"),
-    //            @CacheEvict(value = "searchRecords", allEntries = true) })
+	// @Caching(evict = { @CacheEvict(value = "numberOfAllRecords", allEntries =
+	// true),
+	// @CacheEvict(value = "dictionary", key = "#aVocName"),
+	// @CacheEvict(value = "allRecordIdsFromVoc", key = "#aVocName"),
+	// @CacheEvict(value = "recordCount", key = "#aVocName"),
+	// @CacheEvict(value = "allSortedVocabloryRecords", key = "#aVocName"),
+	// @CacheEvict(value = "searchRecords", allEntries = true) })
 
-    @Override
-    @Caching(evict = { @CacheEvict(value = "numberOfAllRecords", allEntries = true),
-            @CacheEvict(value = "dictionary", key = "#aVocName"),
-            @CacheEvict(value = "recordCount", key = "#aVocName"),
-            @CacheEvict(value = "searchRecords", allEntries = true),
-            @CacheEvict(value = "allSortedVocabloryRecords", key = "#aVocName") })
-    public boolean addRecordToVoc(String aVocName, Long aContent) {
+	@Override
+	@Caching(evict = {
+			@CacheEvict(value = "numberOfAllRecords", allEntries = true),
+			@CacheEvict(value = "dictionary", key = "#aVocName"),
+			@CacheEvict(value = "recordCount", key = "#aVocName"),
+			@CacheEvict(value = "searchRecords", allEntries = true),
+			@CacheEvict(value = "allSortedVocabloryRecords", key = "#aVocName") })
+	public boolean addRecordToVoc(String aVocName, Long aContent) {
 
-        try {
-            Dictionary dictionary = dictionaryService_i.getDictionary(aVocName);
-            dictionary.addRecord(aContent.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
+		try {
+			Dictionary dictionary = dictionaryService_i.getDictionary(aVocName);
+			dictionary.addRecord(aContent.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
 
-    @Override
-    @Cacheable("searchRecords")
-    public List<VocabloryRecord> searchRecordsLessThan(String aVocName, Long aLessThan) throws Exception {
+	@Override
+	@Cacheable("searchRecords")
+	public List<VocabloryRecord> searchRecordsLessThan(String aVocName,
+			Long aLessThan) throws Exception {
 
-        List<VocabloryRecord> allRecords = vocabloryRecordService_i.getGetAllSortedVocabloryRecords(aVocName);
+		List<VocabloryRecord> allRecords = vocabloryRecordService_i
+				.getGetAllSortedVocabloryRecords(aVocName);
 
-        int i;
-        for (i = 0; i < allRecords.size(); i++) {
-            VocabloryRecord vocabloryRecord = allRecords.get(i);
-            Long recContent = vocabloryRecord.getContent();
-            if (recContent >= aLessThan)
-                break;
-        }
+		int i = 0;
+		for (VocabloryRecord vocabloryRecord : allRecords) {
+			if (vocabloryRecord.getContent() >= aLessThan)
+				break;
+			i++;
+		}
 
-        return allRecords.subList(0, i);
-    }
+		return allRecords.subList(0, i);
+	}
 }
